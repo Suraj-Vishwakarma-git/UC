44a31c0dd645757cb0abbf8cf44b46be65ba4249
Suraj <ssvsurajvishwakarma@gmail.com>
2026-07-20 23:48:35 +0530

    Task1

---
+++ task1/src.js
@@ -0,0 +1,12 @@
+let arr=[
+"White",
+"Pinkman",
+"Fring",
+"Schrader",
+"Salamanca",
+"Ehrmantraut",
+"Goodman"
+]
+
+let name="lo";
+console.log(name.substring(0,4)+" "+arr[Math.floor(Math.random()*7)]); 
